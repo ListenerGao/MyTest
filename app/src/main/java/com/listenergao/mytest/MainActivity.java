33ae53430ae0a918a4f551253238cf759ac5e5a3
ListenerGao <hnthgys@gmail.com>
2016-06-29 17:23:04 +0800

    添加侧滑菜单的头布局

--- app/src/main/java/com/listenergao/mytest/MainActivity.java
+++ app/src/main/java/com/listenergao/mytest/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.widget.FrameLayout;
 
 import com.listenergao.mytest.activity.BaseActivity;
+import com.listenergao.mytest.fragment.LeftDrawerFragment;
 import com.listenergao.mytest.fragment.MainFragment;
 
 import butterknife.BindView;
@@ -13,11 +14,18 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
     /**
-     * 主页面的帧布局
+     * 主页面
      */
     @BindView(R.id.content_frame)
     private FrameLayout content_frame;
+    /**
+     * 侧拉菜单
+     */
+    @BindView(R.id.left_drawer)
+    private FrameLayout left_drawer;
 
+    //Fragment标识
+    private static final String TAG_MAIN_MENU ="TAG_MAIN_MENU" ;
     private static final String TAG_LEFT_MENU = "TAG_LEFT_MENU";
 
     @Override
@@ -44,7 +52,8 @@ public class MainActivity extends BaseActivity {
     private void initFragment() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();   //开启事务
-        fragmentTransaction.replace(R.id.content_frame,new MainFragment(),TAG_LEFT_MENU);
+        fragmentTransaction.replace(R.id.content_frame,new MainFragment(),TAG_MAIN_MENU);   //主页面
+        fragmentTransaction.replace(R.id.left_drawer,new LeftDrawerFragment(),TAG_LEFT_MENU);//侧拉菜单页
         fragmentTransaction.commit();   //提交事务
     }
 
---
+++ app/src/main/java/com/listenergao/mytest/fragment/LeftDrawerFragment.java
@@ -0,0 +1,23 @@
+package com.listenergao.mytest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.listenergao.mytest.R;
+
+/**
+ * Created by admin on 2016/6/29.
+ * 侧拉菜单界面
+ */
+public class LeftDrawerFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_left_menu,null);
+        return view;
+    }
+}
