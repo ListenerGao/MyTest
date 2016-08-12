bf297d3d59e358699187ef9bd3944283d8812f07
ListenerGao <hnthgys@gmail.com>
2016-08-12 19:34:24 +0800

    添加设置页面

--- app/src/main/java/com/listenergao/mytest/activity/SettingsActivity.java
+++ app/src/main/java/com/listenergao/mytest/activity/SettingsActivity.java
@@ -1,11 +1,32 @@
 package com.listenergao.mytest.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.listenergao.mytest.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by admin on 2016/8/11.
  */
 public class SettingsActivity extends BaseActivity {
+    @BindView(R.id.tv_version_number)
+    TextView mVersionNumber;
+    @BindView(R.id.tv_update)
+    TextView mUpdate;
+    @BindView(R.id.ll_version_update)
+    LinearLayout mVersionUpdate;
+    @BindView(R.id.ll_function_introduction)
+    LinearLayout mFunctionIntroduction;
+    @BindView(R.id.ll_help)
+    LinearLayout mHelp;
+
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_settings;
@@ -13,11 +34,26 @@ public class SettingsActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        ButterKnife.bind(this);
     }
 
     @Override
     protected void initData() {
 
+    }
+
+
+    @OnClick({R.id.ll_version_update, R.id.ll_function_introduction, R.id.ll_help})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_version_update:
+                break;
+            case R.id.ll_function_introduction:
+                Toast.makeText(this, "功能介绍", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ll_help:
+                Toast.makeText(this, "帮助", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/listenergao/mytest/utils/UiUtils.java
+++ app/src/main/java/com/listenergao/mytest/utils/UiUtils.java
@@ -1,6 +1,8 @@
 package com.listenergao.mytest.utils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import com.listenergao.mytest.BaseApplication;
@@ -12,9 +14,10 @@ public class UiUtils {
 
     /**
      * 对外提供上下文
+     *
      * @return
      */
-    public static Context getContext(){
+    public static Context getContext() {
         return BaseApplication.getApplication();
     }
 
@@ -22,9 +25,9 @@ public class UiUtils {
      * 根据手机分辨率,将dp转换为px(像素)
      * density值表示每英寸有多少个显示点
      */
-    public static int dip2px(Context context,float dpValue){
+    public static int dip2px(Context context, float dpValue) {
         float scale = context.getResources().getDisplayMetrics().density;
-        Log.d("TAG","dip2px scale=" + scale);
+        Log.d("TAG", "dip2px scale=" + scale);
         return (int) (dpValue * scale + 0.5f);
     }
 
@@ -32,10 +35,39 @@ public class UiUtils {
      * 根据手机分辨率,将px转换为dp
      * density值表示每英寸有多少个显示点
      */
-    public static int px2dip(Context context,float pxValue) {
+    public static int px2dip(Context context, float pxValue) {
         float scale = context.getResources().getDisplayMetrics().density;
-        Log.d("TAG","px2dip scale=" + scale);
+        Log.d("TAG", "px2dip scale=" + scale);
         return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    /**
+     * 获取版本名称
+     *
+     * @param context
+     * @return
+     * @throws PackageManager.NameNotFoundException
+     */
+    public static String getVersionName(Context context) throws PackageManager.NameNotFoundException {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+        String versionName = packageInfo.versionName;
+        return versionName;
+    }
+
+    /**
+     * 获取版本号
+     *
+     * @param context
+     * @return
+     * @throws PackageManager.NameNotFoundException
+     */
+    public static int getVersionCode(Context context) throws PackageManager.NameNotFoundException {
+        PackageManager packageManager = context.getPackageManager();
+        PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+        int versionCode = packageInfo.versionCode;
+        return versionCode;
     }
 
 
