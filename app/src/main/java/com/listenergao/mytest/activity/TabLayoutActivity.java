66bc5c11cac7cd3bb74be3ffe066f404b5a05bc7
ListenerGao <hnthgys@gmail.com>
2016-10-14 18:31:44 +0800

    完成简单TabLayout示例

--- app/src/main/java/com/listenergao/mytest/activity/TabLayoutActivity.java
+++ app/src/main/java/com/listenergao/mytest/activity/TabLayoutActivity.java
@@ -1,5 +1,6 @@
 package com.listenergao.mytest.activity;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.widget.LinearLayout;
 
@@ -15,6 +16,8 @@ public class TabLayoutActivity extends BaseActivity {
     Toolbar toolbar;
     @BindView(R.id.activity_tab_layout)
     LinearLayout activityTabLayout;
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
 
     @Override
     protected int getLayoutResId() {
@@ -28,6 +31,17 @@ public class TabLayoutActivity extends BaseActivity {
         toolbar.setTitle("TabLayout");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //添加8个Tab标签，并设置第一个Tab标签为选中状态.
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"),true);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 5"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 6"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 7"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 8"));
+        //设置Tab的模式为可滑动，当tab标签超过屏幕宽度时，可以滑动.
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
     }
 
     @Override
